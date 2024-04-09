3a23322e537ba3981ef847619802be20297726e2
ecomnazar <ecomnazar.me@gmail.com>
2024-04-09 15:29:59 +0500

    [completed]: certificate

--- server/src/user/lib/compositeImage.ts
+++ server/src/user/lib/compositeImage.ts
@@ -2,22 +2,37 @@ import * as sharp from 'sharp';
 import * as fs from 'fs-extra';
 
 export const compositeImage = async (fullname: string, userId: string) => {
-  const width = 600;
+  const width = 3200;
   const height = 600;
   const date = new Date().getTime();
+  const currentDate = new Date();
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  const dateStringWithMonthName = currentDate.toLocaleString(
+    'default',
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    options,
+  );
   const svgText = `
     <svg width="${width}" height="${height}">
       <style>
-        .title { fill: #5D604B; font-size: 35px}
+        .title { fill: #9553FF; font-size: 185px;}
       </style>
       <text x="45%" y="40%" text-anchor="middle" class="title">${fullname}</text>
     </svg>`;
   const idText = `
     <svg width="${width}" height="${height}">
       <style>
-        .title { fill: #5D604B; font-size: 35px}
+        .title { fill: #5D604B; font-size: 48px}
       </style>
-      <text x="45%" y="40%" text-anchor="middle" class="title">${userId}</text>
+      <text x="45%" y="40%" text-anchor="middle" class="title">${`Certificate serial number: undp-SCGCG-${userId}`}</text>
+    </svg>`;
+  const timeText = `
+    <svg width="${width}" height="${height}">
+      <style>
+        .title { fill: #9553FF; font-size: 85px; font-style: italic}
+      </style>
+      <text x="45%" y="40%" text-anchor="middle" class="title">${dateStringWithMonthName}</text>
     </svg>`;
 
   const folderPath = __dirname + '/processed_images';
@@ -40,8 +55,9 @@ export const compositeImage = async (fullname: string, userId: string) => {
 
   sharp('./certificate.jpg')
     .composite([
-      { input: Buffer.from(svgText), left: 180, top: 65 },
-      { input: Buffer.from(idText), left: 180, top: 165 },
+      { input: Buffer.from(svgText), left: 360, top: 895 },
+      { input: Buffer.from(idText), left: 285, top: 2085 },
+      { input: Buffer.from(timeText), left: 295, top: 1740 },
     ])
     .toFile(__dirname + `/processed_images/${date}.jpg`);
   return __dirname + `/processed_images/${date}.jpg`;
